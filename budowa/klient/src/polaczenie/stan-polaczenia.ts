f8fb383500f117a36fdcd3c58896ca7a3012ff77
Dariusz Naharnowicz <dnaharnowicz@danaco-group.pl>
2026-08-29 21:51:56 +0000

    Usuwa reszte kodu klienta pisanego od nowa

--- budowa/klient/src/polaczenie/stan-polaczenia.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Stan warstwy połączenia widziany przez pozostałe warstwy klienta. Stan jest
- * wyłącznie informacją: rozłączenie nie odbiera warstwom wyższym prawa do
- * wysyłania.
- */
-export type StanPolaczenia = 'rozlaczony' | 'laczenie' | 'polaczony' | 'ponawianie';
